efd1274dd94c656dbd01d70354de852f3e177147
mariaelguli-hub <mariaelguli@gmail.com>
2026-08-31 12:05:16 +0100

    Update supabase.js

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://gziyhxhoswkidegileif.supabase.co'
-const supabaseAnonKey = 'sb_publishable_jURuFv2g1JN8PpXzuVtQmw_5jNfiQAX'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6aXloeGhvc3draWRlZ2lsZWlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgxMzYzMzMsImV4cCI6MjEwMzcxMjMzM30.x5VOiguA6Gta3Bxi9Iablb3oICpi1AGtxzr3xl7vw8Q'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
